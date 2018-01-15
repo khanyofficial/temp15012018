72691c7a44927b7a8be5309ddac67d2d3bf8a1ed
khanyofficial <35334438+khanyofficial@users.noreply.github.com>
2018-01-15 12:25:31 +0530

    Create 2.py
    test

---
+++ 2.py
@@ -0,0 +1,13 @@
+@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+import numpy as np
+
+
+n1 = np.array(np.random.random(size=2*2))
+print n1
